0e3693aa9bcb34b80707442670c9bd8f844f118a
Laurence Cartwright <muzoedge@gmail.com>
2017-01-14 22:54:07 +0000

    WIP Text segment grouping algorithm

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var path = require("path");
 var jquery = fs.readFileSync("./node_modules/jquery/dist/jquery.slim.js", "utf-8"); 
 var PDFParser = require("pdf2json");
 var source = process.env.source;
-// var querystring = require('querystring');
 
 if (source == "URL") {
   jsdom.env({
@@ -84,11 +83,13 @@ function parsePDFData(pdfData) {
   var page = pdfData["formImage"]["Pages"][0];
   var texts = [];
   // Flatten text arrays, remove unused attributes
+  var count = 0;
   page["Texts"].forEach(function(e) {
     var n = {};
-    n.xStart = e.x;
-    n.xEnd = (parseFloat(e.x) +  parseFloat(e.w)).toFixed(3);
-    n.y = e.y;
+    n.id = count++;
+    n.xStart = parseFloat(parseFloat(e.x).toFixed(3));
+    n.xEnd = parseFloat((parseFloat(e.x) + parseFloat(e.w)).toFixed(3));
+    n.y = parseFloat(parseFloat(e.y).toFixed(3));
     n.w = e.w;
     n.text = "";
     e.R.forEach(function(t) {
@@ -97,5 +98,74 @@ function parsePDFData(pdfData) {
     n.text = decodeURIComponent(n.text);
     texts.push(n);
   }, this);
-  console.log(JSON.stringify(texts));
+  // console.log(JSON.stringify(texts));
+
+  // array of unique yVals
+  var yVals = [];
+  function pushIfNotExits(array, element) {
+    if (array.indexOf(element) === -1) {
+      array.push(element);
+    }
+  }
+
+  texts.forEach(function(e) {
+    pushIfNotExits(yVals, e.y);
+  }, this);
+
+  // console.log(yVals.length);
+  // console.log(yVals[4]);
+  // console.log(JSON.stringify(yVals));
+
+  var yLine = [];
+  texts.forEach(function(e){
+    if (e.y == 13.446) {
+      yLine.push(e);
+    }
+  }, this);
+
+  var yLineNext = true;
+  var yIds = [];
+
+  yLine.forEach(function(e){
+    yIds.push(e.id);
+  }, this);
+
+  function getById(array, id) {
+    for (var i = 0; i < array.length; i++) {
+      if (array[i].id == id) {
+        return array[i];
+      }
+    }
+  }
+
+  while (yLineNext) {
+    var group = {};
+    group.texts = [];
+    var nextYIds = [];
+    for(var i = 0; i < yIds.length; i++) {
+      var id = yIds[i];
+      if (i == 0) {
+        group.texts.push(getById(yLine, id));
+      }
+    }
+    yIds.forEach(function(id) {
+      
+    }, this);
+  }
+
+  // Read first element, 
+  //    add to new object
+  //    remove from pool of id's to process
+  // read next element then check
+  //    is the ID consecutive
+  //    does the text overlap/touch
+  //      add to object array
+  //      remove from pool of id's to process
+  // continue reading elements until array is exhausted
+  // start from step 1 again, with the new pool of id's
+  // might be easier to just add the next round of id's to the array
+
+
+  console.log(JSON.stringify(yLine));
+
 }
